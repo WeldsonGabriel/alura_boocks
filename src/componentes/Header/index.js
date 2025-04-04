e0f5ae0486369c86bce4bbd3bd395c604a3557e2
Gabriel Borges <gabrielborgesw2@gmail.com>
2025-04-04 08:52:08 -0300

    "retirando os css"

--- src/componentes/Header/index.js
+++ src/componentes/Header/index.js
@@ -1,15 +1,20 @@
-import './estilo.css'
 import IconesHeader from '../Icones/index';
 import Logo from '../Logo/index'
 import OpcoesHeader from '../OpcoesHeader/index';
+import styled from 'styled-components'
 
+const HeaderContainer = styled.header` 
+    background-color: #FFF;
+    display: flex;
+    justify-content: center;
+`
 function Header(){    
     return(
-        <header className='App-header'>
+        <HeaderContainer>
             <Logo/>
             <OpcoesHeader/>
             <IconesHeader/>
-        </header>
+        </HeaderContainer>
         
     )
 }
